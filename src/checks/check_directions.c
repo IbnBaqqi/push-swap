7f316b8582531fb938f08a794a155e89c0a03fcb
Abdulbaki <Salausmart132@gmail.com>
2025-01-02 09:06:47 +0200

    sorting three

--- src/checks/check_directions.c
+++ src/checks/check_directions.c
@@ -6,7 +6,7 @@
 /*   By: sabdulba <sabdulba@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 09:26:01 by sabdulba          #+#    #+#             */
-/*   Updated: 2025/01/01 09:49:15 by sabdulba         ###   ########.fr       */
+/*   Updated: 2025/01/02 09:02:11 by sabdulba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,23 @@ int	check_back(t_node *min_node, t_node **stack_a)
 	if (last_node(*stack_a)->value == temp->value)
 		return (1);
 	return (0);
+}
+
+int	check_cyclic(t_node **stack_a)
+{
+	t_node_details	*min;
+
+	min = min_details(stack_a);
+	if (min->node->next && min->node->prev)
+	{
+		if (((*stack_a)->value < last_node(*stack_a)->value)
+			|| (*stack_a)->value == min->value)
+			return (free(min), 0);
+	}
+	if (min->node->prev && !check_front(min->node, stack_a))
+		return (free(min), 0);
+	if (min->node->prev && !check_back(min->node, stack_a))
+		return (free(min), 0);
+	free(min);
+	return (1);
 }
