b66bef31df0adb1a599f7c1d4fb64845df0242c4
Salaudeen Abdulbaki <sabdulba@student.hive.fi>
2025-01-05 23:50:47 +0200

    quick sorting

--- src/sort/quick_sort.c
+++ src/sort/quick_sort.c
@@ -6,9 +6,27 @@
 /*   By: sabdulba <sabdulba@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 00:31:12 by sabdulba          #+#    #+#             */
-/*   Updated: 2025/01/05 00:31:26 by sabdulba         ###   ########.fr       */
+/*   Updated: 2025/01/05 23:35:55 by sabdulba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void 	ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	partition(int *arr)
+{
+	
+}
+
+void	quick_sort(char *array)
+{
+	
+}
